7a986e9f180ece665cf614daaac5dc404056dacd
jacobmartin26 <121983988+jacobmartin26@users.noreply.github.com>
2023-01-25 20:12:00 -0500

    Add files via upload

---
+++ Guessing Dice.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int userProduct, userSum, userSumOfSq;
+    bool found = 0;
+
+    while(found == 0)
+    {
+        printf("\nWhat is the product of your numbers?\n");
+        scanf("%d", &userProduct);
+
+        printf("\nWhat is the sum of your numbers?\n");
+        scanf("%d", &userSum);
+
+        userSumOfSq = userProduct;
+
+        int d1, d2, d3, d4;
+        int myProduct, mySum, mySumOfSq;
+
+        for(d1 = 1; d1 <= 6; d1++)
+        {
+            for(d2 = 1; d2 <= 6; d2++)
+            {
+                for(d3 = 1; d3 <= 6; d3++)
+                {
+                    for(d4 = 1; d4 <= 6; d4++)
+                    {
+                        myProduct = d1 * d2 * d3 * d4;
+                        mySum = d1 + d2 + d3 + d4;
+                        mySumOfSq = (d1 * d1) + (d2 * d2) + (d3 * d3) + (d4 * d4);
+
+                        if(myProduct == userProduct && mySum == userSum && mySumOfSq != userSumOfSq)
+                        {
+                            printf("\nYour numbers are %d %d %d %d\n", d1, d2, d3, d4);
+                            return 0;
+                        }
+                    }
+                }
+            }
+        }
+        printf("\nCould not find a valid dice combination for the given parameters, please try different ones.\n");
+    }
+
+    return 0;
+}
